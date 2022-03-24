868aa21b836e0b028bcff80b8558edec7908f7f3
turkuazsengul <turkuazsengul@gmail.com>
2022-03-24 20:59:07 +0300

    mikro servis içerisinde yer alan api lerin otomatik test projesi

---
+++ src/main/java/com/example/apiotomation/Service/QaRunMethodsService.java
@@ -0,0 +1,8 @@
+package com.example.apiotomation.Service;
+
+import com.example.apiotomation.Modal.RequestModal;
+import com.example.apiotomation.Modal.TestResultModal;
+
+public interface QaRunMethodsService {
+    void runMethodTypeGet(RequestModal requestModal, TestResultModal testResultModel);
+}
